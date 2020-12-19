2e786d87d1d26be3158aca2702b5cd9db296d99c
erika cueva <ecueva88@gmail.com>
2020-12-19 10:31:08 -0800

    updated API routes for POST rrequest

--- Routes/apiRoutes.js
+++ Routes/apiRoutes.js
@@ -4,6 +4,7 @@ const notes = require("../db/db.json");
 const fs = require("fs");
 const { v4: uuidv4 } = require('uuid');
 
+
 // routing
 module.exports = function (app) {
   // API GET request
@@ -15,18 +16,28 @@ module.exports = function (app) {
 
   app.post("/api/notes", function (req, res) {
     console.log(req.body);
-    req.body.id = uuidv4();
+    const newNote = req.body;
+    newNote.id = uuidv4();
     notes.push(req.body);
+    fs.writeFile('notes.html', JSON.stringify(notes), (err) => {
+      if (err) throw err
+    });
     // fs.writeFile to overwrite your notes in the db.json file, week9, activiyt 13???
     // put res.json(notes) inside fs.writeFile because of asynchronicity
-    res.json(notes);
+    res.json(JSON.stringify(notes));
   });
 
   // code to clear notes
   app.delete("/api/notes/:id", function (req, res) {
       console.log(req.params.id);
       //fs.readFile to get notes
-      // for loop to get rid of note that matches the id
+      // for loop to get rid of note that matches the id - .filter() out id
       // fs.writeFile to overwrite your db.json file with the notes array that no longer has that note
   })
 };
+
+//get the data - readFile
+
+//modify the data - filter or ad new note
+
+//write the notes back to db- writeFile
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
@@ -7,7 +8,7 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // tells express where static assets are (CSS and front end JS and images)
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname,"/public")));
 
 require("./Routes/apiRoutes")(app);
 require("./Routes/htmlRoutes")(app);
