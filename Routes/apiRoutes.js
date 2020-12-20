31c8ffd3293873baed88746637be683671b7de6d
erika cueva <ecueva88@gmail.com>
2020-12-19 19:00:50 -0800

    worked on delete function

--- Routes/apiRoutes.js
+++ Routes/apiRoutes.js
@@ -2,8 +2,8 @@
 
 const notes = require("../db/db.json");
 const fs = require("fs");
-const { v4: uuidv4 } = require('uuid');
-
+const { v4: uuidv4 } = require("uuid");
+const { json } = require("express");
 
 // routing
 module.exports = function (app) {
@@ -19,25 +19,27 @@ module.exports = function (app) {
     const newNote = req.body;
     newNote.id = uuidv4();
     notes.push(req.body);
-    fs.writeFile('notes.html', JSON.stringify(notes), (err) => {
-      if (err) throw err
+    fs.writeFile("notes.html", JSON.stringify(notes), (err) => {
+      if (err) throw err;
     });
     // fs.writeFile to overwrite your notes in the db.json file, week9, activiyt 13???
     // put res.json(notes) inside fs.writeFile because of asynchronicity
     res.json(JSON.stringify(notes));
   });
 
-  // code to clear notes
+
   app.delete("/api/notes/:id", function (req, res) {
-      console.log(req.params.id);
-      //fs.readFile to get notes
-      // for loop to get rid of note that matches the id - .filter() out id
-      // fs.writeFile to overwrite your db.json file with the notes array that no longer has that note
-  })
+    let targetNote = req.params.id;
+    notes.splice(targetNote - 1, 1);
+    targetNote((obj, i) => {
+      obj.targetNote = i + 1; 
+    });
+    fs.writeFile("notes.html", JSON.stringify(notes), function(){
+      res.json(notes);
+    });
+  });
+
+  //get the data - readFile
+  //modify the data - filter or ad new note
+  //write the notes back to db- writeFile
 };
-
-//get the data - readFile
-
-//modify the data - filter or ad new note
-
-//write the notes back to db- writeFile
